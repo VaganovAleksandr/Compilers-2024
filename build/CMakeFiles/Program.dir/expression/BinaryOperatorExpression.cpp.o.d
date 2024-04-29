fbcbf221cecc0ddba77fd77ff7f3160b0d32a16f
Alexander Vaganov <vaganov.am@phystech.edu>
2024-04-29 10:07:08 +0300

    delete unnecessary files

--- build/CMakeFiles/Program.dir/expression/BinaryOperatorExpression.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Program.dir/expression/BinaryOperatorExpression.cpp.o: \
-  /Users/aleksandrvaganov/Desktop/Compilers/expression/BinaryOperatorExpression.cpp \
-  /Users/aleksandrvaganov/Desktop/Compilers/expression/BinaryOperatorExpression.h \
-  /Users/aleksandrvaganov/Desktop/Compilers/expression/Expression.h
--- build/CMakeFiles/Program.dir/expression/IntegerExpression.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Program.dir/expression/IntegerExpression.cpp.o: \
-  /Users/aleksandrvaganov/Desktop/Compilers/expression/IntegerExpression.cpp \
-  /Users/aleksandrvaganov/Desktop/Compilers/expression/IntegerExpression.h \
-  /Users/aleksandrvaganov/Desktop/Compilers/expression/Expression.h
