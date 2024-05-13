2804394fc71e160f0c9847afde9fe585cf6b36bf
Alexander Vaganov <vaganov.am@phystech.edu>
2024-05-13 03:30:40 +0300

    bug fix

--- Driver.h
+++ Driver.h
@@ -1,16 +1,18 @@
 #ifndef Driver_H
 #define Driver_H
 
-#include <map>
 #include <any>
+#include <map>
+#include <string>
 
 class Driver {
  public:
   std::map<std::string, int> variables_int = {};
   std::map<std::string, bool> variables_bool = {};
   std::map<std::string, std::string> variables_string = {};
-  //TODO: Default constructors support
-  void AddVariable(const std::string& type, const std::string& name, const std::any& value);
+  // TODO: Default constructors support
+  void AddVariable(const std::string& type, const std::string& name,
+                   const std::any& value);
   void ChangeVariable(const std::string& name, const std::any& new_value);
   [[nodiscard]] std::any GetVariable(const std::string& name) const;
   void Clear();
